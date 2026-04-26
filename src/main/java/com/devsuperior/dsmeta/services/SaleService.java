5fbedbea67dd22019835cf798c055b0b0ff78599
gmtop24 <gmtopan2@gmail.com>
2026-04-26 11:21:16 -0300

    Project forked, open-in-view=false, first test

--- src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsmeta.dto.SaleMinDTO;
 import com.devsuperior.dsmeta.entities.Sale;
@@ -15,6 +16,7 @@ public class SaleService {
 	@Autowired
 	private SaleRepository repository;
 	
+	@Transactional(readOnly = true)
 	public SaleMinDTO findById(Long id) {
 		Optional<Sale> result = repository.findById(id);
 		Sale entity = result.get();
